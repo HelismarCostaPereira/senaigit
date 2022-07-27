783d11f1099d3975d3cc21431f88a8bccb6aa9eb
helismar costa pereira <helismar.c.pereira@outlook.com>
2022-07-27 18:45:02 -0300

    Cadastro de peças SAVINIS

---
+++ cadastrosavinis.js
@@ -0,0 +1,24 @@
+// Variáveis, são um espaço na memória do computador para guardar algum valor.
+// (*È utilizado barras em JS, podendo variar dependendo da linguagem utilizada)
+console.log("sistema de cadastro de peças")
+
+let listadePeca = ["Faról", "buzina", "Para Choque", "embreagem", "Cinto segurança", "Radiaidor",
+   "Disco de freio", "Filtro de óleo", "Pistão", "Volante" ];
+if(listadePeca => 10){
+ console.log ("não mais espaço para cadastro")
+}else{
+ console.log("ainda a espaço para o cadastro de mais peças")
+ }
+var peso = 100
+  console.log("Lista de Peças")
+ if(pesoPeca > 100){
+  console.log("Cadastro permitido")
+ }else{
+  console.log("Peso insuficiente, não é possível cadastrar")
+ }
+var nomePeca = "Faról"
+if (nomePeca.length < 3) {
+    console.log("Nome menor do que o permitido!")
+} else {
+    console.log("Cadastrado feito com sucesso!")
+}
